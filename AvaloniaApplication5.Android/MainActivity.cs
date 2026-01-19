7d3d2788c79c2b6cb64a5f66276d4925b01791fe
roadtoseniors <Mariapoyua@yandex.ru>
2026-01-19 21:10:59 +0300

    first commit

---
+++ AvaloniaApplication5.Android/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Avalonia;
+using Avalonia.Android;
+
+namespace AvaloniaApplication5.Android;
+
+[Activity(
+    Label = "AvaloniaApplication5.Android",
+    Theme = "@style/MyTheme.NoActionBar",
+    Icon = "@drawable/icon",
+    MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+public class MainActivity : AvaloniaMainActivity<App>
+{
+    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+    {
+        return base.CustomizeAppBuilder(builder)
+            .WithInterFont();
+    }
+}
---
+++ AvaloniaApplication5/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AvaloniaApplication5.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
